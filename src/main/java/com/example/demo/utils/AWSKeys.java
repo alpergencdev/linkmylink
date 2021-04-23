a0bc90d2caa6e5829cad274e684b8831b299535b
Mehmet Alper Genç <m.alper.genc@gmail.com>
2021-04-23 14:48:32 +0300

    database part complete, more testing could be nice

---
+++ src/main/java/com/example/demo/utils/AWSKeys.java
@@ -0,0 +1,7 @@
+package com.example.demo.utils;
+
+public class AWSKeys {
+
+    public static final String ACCESS_KEY = "ACCESS_KEY_HERE";
+    public static final String SECRET_KEY = "SECRET_ACCESS_KEY_HERE";
+}
